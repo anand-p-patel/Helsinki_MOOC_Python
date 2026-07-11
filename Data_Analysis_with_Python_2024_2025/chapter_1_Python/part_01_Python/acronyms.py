4505ce258c8b2cc5f0be4f8515a62582db0a78ac
slappyjackson <103470591+slappyjackson@users.noreply.github.com>
2026-07-10 21:24:50 -0500

    Create acronyms.py

---
+++ Data_Analysis_with_Python_2024_2025/chapter_1_Python/part_01_Python/acronyms.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import string
+
+def acronyms(s):
+    acro_list = []
+    for word in s.split():
+        stripped_word = word.strip(string.punctuation)
+        if len(stripped_word) >= 2 and stripped_word.isupper():
+            acro_list.append(stripped_word)
+
+    return acro_list
+
+def main():
+    print(acronyms("""For the purposes of the EU General Data Protection Regulation (GDPR), the controller of your personal information is International Business Machines Corporation (IBM Corp.), 1 New Orchard Road, Armonk, New York, United States, unless indicated otherwise. Where IBM Corp. or a subsidiary it controls (not established in the European Economic Area (EEA)) is required to appoint a legal representative in the EEA, the representative for all such cases is IBM United Kingdom Limited, PO Box 41, North Harbour, Portsmouth, Hampshire, United Kingdom PO6 3AU."""))
+
+
+if __name__ == "__main__":
+    rint(acronyms("""For the purposes of the EU General Data Protection Regulation (GDPR), the controller of your personal information is International Business Machines Corporation (IBM Corp.), 1 New Orchard Road, Armonk, New York, United States, unless indicated otherwise. Where IBM Corp. or a subsidiary it controls (not established in the European Economic Area (EEA)) is required to appoint a legal representative in the EEA, the representative for all such cases is IBM United Kingdom Limited, PO Box 41, North Harbour, Portsmouth, Hampshire, United Kingdom PO6 3AU."""))
+    main()
