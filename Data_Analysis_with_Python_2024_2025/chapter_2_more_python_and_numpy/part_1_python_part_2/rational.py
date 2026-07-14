bf62918579ae57b5ac82334e0dc54daea7e0d4a7
slappyjackson <103470591+slappyjackson@users.noreply.github.com>
2026-07-14 18:54:32 -0500

    Create rational.py

---
+++ Data_Analysis_with_Python_2024_2025/chapter_2_more_python_and_numpy/part_1_python_part_2/rational.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+class Rational(object):
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+    
+    def __add__(self, other):
+        new_numerator = (self.numerator * other.denominator) + (other.numerator * self.denominator)
+        new_denominator = self.denominator * other.denominator
+        return Rational(new_numerator, new_denominator)
+
+    def __sub__(self, other):
+        new_numerator = (self.numerator * other.denominator) - (other.numerator * self.denominator)
+        new_denominator = self.denominator * other.denominator
+        return Rational(new_numerator, new_denominator)
+    
+    def __mul__(self, other):
+        new_numerator = self.numerator * other.numerator
+        new_denominator = self.denominator * other.denominator
+        return Rational(new_numerator, new_denominator)
+    
+    def __truediv__(self, other):
+        new_numerator = self.numerator * other.denominator
+        new_denominator = self.denominator * other.numerator
+        return Rational(new_numerator, new_denominator)
+    
+    def __eq__(self, other):
+        return self.numerator * other.denominator == other.numerator * self.denominator
+    
+    def __lt__(self, other):
+        return self.numerator * other.denominator < other.numerator * self.denominator
+    
+    def __gt__(self, other):
+        return self.numerator * other.denominator > other.numerator * self.denominator
+    
+    def __str__(self):
+        return f"{self.numerator}/{self.denominator}"
+
+def main():
+    r1=Rational(1,4)
+    r2=Rational(2,3)
+    print(r1)
+    print(r2)
+    print(r1*r2)
+    print(r1/r2)
+    print(r1+r2)
+    print(r1-r2)
+    print(Rational(1,2) == Rational(2,4))
+    print(Rational(1,2) > Rational(2,4))
+    print(Rational(1,2) < Rational(2,4))
+
+if __name__ == "__main__":
+    main()
