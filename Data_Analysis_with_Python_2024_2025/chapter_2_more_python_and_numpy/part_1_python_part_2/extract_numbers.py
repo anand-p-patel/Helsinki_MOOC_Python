1df605c629d87ed25c314916f6616bea6187342e
slappyjackson <103470591+slappyjackson@users.noreply.github.com>
2026-07-14 20:00:25 -0500

    Create extract_numbers.py

---
+++ Data_Analysis_with_Python_2024_2025/chapter_2_more_python_and_numpy/part_1_python_part_2/extract_numbers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def extract_numbers(s):
+    numbers = []
+    for word in s.split():
+        try:
+            numbers.append(int(word))
+        except ValueError:
+            try:
+                numbers.append(float(word))
+            except ValueError:
+                pass
+                    
+    return numbers
+
+def main():
+    print(extract_numbers("abd 123 1.2 test 13.2 -1"))
+
+if __name__ == "__main__":
+    main()
