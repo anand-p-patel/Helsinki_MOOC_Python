7ecd41dc943cfefb8cd70d62553e5648af2a0246
slappyjackson <103470591+slappyjackson@users.noreply.github.com>
2026-07-17 13:36:05 -0500

    exercises

---
+++ Data_Analysis_with_Python_2024_2025/chapter_3_more_mumpy/part_2_matplotlib/multiple_graphs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+
+def main():
+    plt.plot([2,4,6,7], [4,3,5,1])
+    plt.plot([1,2,3,4], [4,2,3,1])
+
+    plt.title("title")
+    plt.xlabel("x-axis")
+    plt.ylabel("y-axis")
+    
+    plt.show()
+
+if __name__ == "__main__":
+    main()
