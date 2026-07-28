b5adf2745d9ace47d97bbd23f3b44f4565e268eb
slappyjackson <103470591+slappyjackson@users.noreply.github.com>
2026-07-28 17:38:20 -0500

    Create split_date_continues.py

---
+++ Data_Analysis_with_Python_2024_2025/chapter_5_stil_more_pandas_and_machine_learning/part_1-pandas_part_3/split_date_continues.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+
+def split_date(df):
+    split_df = df["Päivämäärä"].str.split(expand=True)
+    split_df.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
+
+    weekday_map = {
+        "ma": "Mon", "ti": "Tue", "ke": "Wed", "to": "Thu",
+        "pe": "Fri", "la": "Sat", "su": "Sun"
+    }
+    month_map = {
+        "tammi": 1, "helmi": 2, "maalis": 3, "huhti": 4,
+        "touko": 5, "kesä": 6, "heinä": 7, "elo": 8,
+        "syys": 9, "loka": 10, "marras": 11, "joulu": 12
+    }
+
+    split_df["Weekday"] = split_df["Weekday"].map(weekday_map)
+    split_df["Month"] = split_df["Month"].map(month_map)
+    split_df["Hour"] = split_df["Hour"].str.split(":").str[0]
+
+    split_df = split_df.astype({
+        "Day": int,
+        "Month": int,
+        "Year": int,
+        "Hour": int
+    })
+    return split_df
+
+def split_date_continues():
+    df = pd.read_csv("src/Helsingin_pyorailijamaarat.csv", sep=";")
+    df = df.dropna(how="all", axis=0)
+    df = df.dropna(how="all", axis=1)
+
+    date_df = split_date(df)
+    measurements_df = df.drop(columns=["Päivämäärä"])
+
+    return pd.concat([date_df, measurements_df], axis=1)
+
+def main():
+    df = split_date_continues()
+    print("Shape:", df.shape)
+    print("Column names:\n", df.columns)
+    print(df.head())
+
+
+if __name__ == "__main__":
+    main()
